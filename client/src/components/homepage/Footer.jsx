7e9295ed84143a0914a37c03edbbfc2b8ea6cd8f
DevloperVipinRana <vipinranavip97@gmail.com>
2025-10-27 20:07:14 +0530

    Linking added

--- client/src/components/homepage/Footer.jsx
+++ client/src/components/homepage/Footer.jsx
@@ -3,9 +3,14 @@
 // src/components/homepage/Footer.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Linkedin, Twitter, Instagram, Send } from 'lucide-react';
+import { Twitter, Instagram, Send } from 'lucide-react';
 
 const Footer = () => {
+    const FacebookIcon = () => (
+    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+        <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
+    </svg>
+);
     return (
         <footer className="bg-dark-bg text-text-on-dark py-16 sm:py-20">
             <div className="container mx-auto px-4 sm:px-6">
@@ -77,8 +82,8 @@ const Footer = () => {
                         <a href="#" aria-label="Twitter" className="text-text-on-dark/50 hover:text-primary transition-colors">
                             <Twitter className="w-5 h-5" />
                         </a>
-                        <a href="#" aria-label="LinkedIn" className="text-text-on-dark/50 hover:text-primary transition-colors">
-                            <Linkedin className="w-5 h-5" />
+                        <a href="https://www.facebook.com/profile.php?id=61582072363492" aria-label="Facebook" className="text-text-on-dark/50 hover:text-primary transition-colors">
+                            <FacebookIcon className="w-5 h-5" />
                         </a>
                         <a href="https://www.instagram.com/cetutor_/?igsh=dDV2dGsxZTA0d3d1#" aria-label="Instagram" className="text-text-on-dark/50 hover:text-primary transition-colors">
                             <Instagram className="w-5 h-5" />
